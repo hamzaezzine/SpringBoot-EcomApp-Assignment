c04eed285b03afd7d3e674167240d3b474a549bb
hamzaezzine <ezzinehamza070@gmail.com>
2024-12-28 22:46:34 +0100

    init spring boot project

---
+++ src/test/java/com/api/EcomApp/EcomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.EcomApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
